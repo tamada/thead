6ea868dc0d4cc845f5de3ae46aa24e6394b4346c
akanatr <84696918+akanatr@users.noreply.github.com>
2021-06-07 18:47:01 +0900

    Add files via upload

---
+++ testdata/c_lang/HelloWorld.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("Hello\tWorld!\b?\n");
+
+    return 0;
+}
---
+++ testdata/c_lang/handy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <handy.h>
+
+
+int main(void) {
+    int dadada;
+    HgOpen(600, 480);
+    dadada = aaa(jka);
+HgBox((600-(114*2))/6, (480-114)/3*2, 114, 114);
+    HgBox((600-(114*2))/6*3+114, (480-114)/3*2, 114, 114);
+
+    HgGetChar();
+    HgClose();
+    return 0;
+}
+
+int aaa(jka){
+    int bbb;
+    return calc + clearerr_unlocked;
+    
+}
