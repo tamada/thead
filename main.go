f7740bc8e229e65392ca11cbfa4a8e5a3ef6d0ed
akanatr <tana@tanaka.local>
2021-06-14 17:21:20 +0900

    オプションの実装

--- main.go
+++ main.go
@@ -10,14 +10,14 @@ func helpMessage(originalProgramName string) string {
 	programName := filepath.Base(originalProgramName)
 	return fmt.Sprintf(`%s [OPTIONS]  [FILEs...]
 OPTIONS
-	-c, --bytes <BYTES>      指定された文字数を各ファイルごとに出力する．
-	-n, --lines <LINES>      指定された行数を各ファイルごとに出力する．
-	-q, --quiet              複数ファイルを出力する際の各ファイル名を表示しない．
-	-s, --squeeze-blank      各ファイルごとに連続した空行を1行にする．  
-	-h, --help               このメッセージを出力する.
+	-c, --bytes <BYTES>		指定された文字数を各ファイルごとに出力する．
+	-n, --lines <LINES>		指定された行数を各ファイルごとに出力する．
+	-q, --quiet				複数ファイルを出力する際の各ファイル名を表示しない．
+	-s, --squeeze-blank		各ファイルごとに連続した空行を1行にする．  
+	-h, --help				このメッセージを出力する.
 ARGUMENTS
-	FILEs...                 カウント対象を指定する．theadはzip/tar/tar.gz/tar.bz2/jar/warファイルを受け付ける．
-	DIRs...                  指定したディレクトリ内のファイルを入力ファイルとする．`, programName)
+	FILEs...				カウント対象を指定する．theadはzip/tar/tar.gz/tar.bz2/jar/warファイルを受け付ける．
+	DIRs...					指定したディレクトリ内のファイルを入力ファイルとする．`, programName)
 }
 
 func goMain(args []string) int {
