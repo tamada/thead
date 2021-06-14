7306e119975d6a7a007789e249ace32279ae6f94
akanatr <tana@tanaka.local>
2021-06-14 17:15:31 +0900

    オプションの実装

--- main_test.go
+++ main_test.go
@@ -5,12 +5,12 @@ func Example_help() {
 	// Output:
 	// thead [OPTIONS]  [FILEs...]
 	// OPTIONS
-	// 	-c, --bytes <BYTES>		指定された文字数を各ファイルごとに出力する．
-	// 	-n, --lines <LINES>		指定された行数を各ファイルごとに出力する．
-	// 	-q, --quiet				複数ファイルを出力する際の各ファイル名を表示しない．
-	// 	-s, --squeeze-blank		各ファイルごとに連続した空行を1行にする．
-	// 	-h, --help				このメッセージを出力する.
+	// 	-c, --bytes <BYTES>      指定された文字数を各ファイルごとに出力する．
+	// 	-n, --lines <LINES>      指定された行数を各ファイルごとに出力する．
+	// 	-q, --quiet              複数ファイルを出力する際の各ファイル名を表示しない．
+	// 	-s, --squeeze-blank      各ファイルごとに連続した空行を1行にする．
+	// 	-h, --help               このメッセージを出力する.
 	// ARGUMENTS
-	// 	FILEs...				カウント対象を指定する．theadはzip/tar/tar.gz/tar.bz2/jar/warファイルを受け付ける．
-	// 	DIRs...					指定したディレクトリ内のファイルを入力ファイルとする．
+	// 	FILEs...                 カウント対象を指定する．theadはzip/tar/tar.gz/tar.bz2/jar/warファイルを受け付ける．
+	// 	DIRs...                  指定したディレクトリ内のファイルを入力ファイルとする．
 }
